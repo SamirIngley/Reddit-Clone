3e47a3128fdfa8bcf6f648087e7267024b782977
SamirIngley <samir.ingle7@gmail.com>
2020-07-19 16:56:05 -0700

    nav bar etc init

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 // const methodOverride = require('method-override')
 const app = express()
+const exphbs = require('express-handlebars');
+
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
@@ -9,7 +11,7 @@ app.set('view engine', 'handlebars');
 
 
 app.get('/', (req,res) => {
-    res.render('main', {}) 
+    res.render('home', {msg: 'hello world'}) 
 })
 
 
