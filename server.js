acbf82633e95bf0c867f50ca5b4a8c2049b01eda
SamirIngley <samir.ingle7@gmail.com>
2020-07-19 15:43:40 -0700

    handlebars init not working

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 const express = require('express')
 // const methodOverride = require('method-override')
 const app = express()
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
 
 app.get('/', (req,res) => {
-    res.send('Hello World!') 
+    res.render('main', {}) 
 })
 
 
